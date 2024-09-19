a229bf39cdd099df9d964a9df754b21add8c0945
IONHAX04 <thirukumara04@gmail.com>
2024-09-19 10:52:26 +0530

    Images Updated for discover page

--- src/components/10-Discover/Discover.jsx
+++ src/components/10-Discover/Discover.jsx
@@ -1,8 +1,15 @@
+import "./Discover.css";
 
 export default function Discover() {
   return (
     <div>
-      
+      <div className="discoverClub">
+        <div className="discoverClubIntro"></div>
+        <div className="tableExperience"></div>
+        <div className="stripShow"></div>
+        <div className="privateDance"></div>
+        <div className="partyGroup"></div>
+      </div>
     </div>
-  )
+  );
 }
