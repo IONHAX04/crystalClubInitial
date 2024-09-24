5cf4b68b59d940d7867af4b9f98cf737bb17b5df
IONHAX04 <thirukumara04@gmail.com>
2024-09-24 13:46:31 +0530

    English

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { I18nextProvider } from "react-i18next";
 
 i18next.init({
   interpolation: { escapeValue: false },
-  lng: "de",
+  lng: "en",
   resources: {
     de: {
       global: global_de,
