9c494c41e7f99afb02edebe53dfa7eb8b55afa90
IONHAX04 <thirukumara04@gmail.com>
2024-09-24 22:26:53 +0530

    Latesst changes updated

--- src/components/03-Join/Join.jsx
+++ src/components/03-Join/Join.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
-import "./Join.css"
+import "./Join.css";
 
 export default function Join() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
--- src/components/14-Events/Events.jsx
+++ src/components/14-Events/Events.jsx
@@ -17,6 +17,7 @@ const eventData = [
     content:
       "Join us for a terrifyingly good time at our Halloween Party! The club will be transformed into a haunted house filled with spooky decor, eerie lighting, and fog-filled dance floors. Wear your scariest, funniest, or most creative costume and compete in our epic costume contest with amazing prizes for the best outfits. Dance the night away to haunting beats by our live DJ, enjoy special Halloween-themed cocktails, and capture the moment in our ghoulish photo booth. It's going to be a night full of thrills, chills, and unforgettable memories. Get your tickets now and be part of the frightful fun!",
     img: eventImg2,
+    date: "October 31, 2024",
   },
   {
     id: 2,
@@ -26,6 +27,7 @@ const eventData = [
     content:
       "Get ready for the most exciting party of the year! With top DJs spinning the latest hits, themed decorations, and an electric atmosphere, this event will be the highlight of the season. Book your tickets now and prepare for an evening of non-stop fun with special performances and surprise giveaways!",
     img: eventImg1,
+    date: "November 5, 2024",
   },
   {
     id: 3,
@@ -35,6 +37,7 @@ const eventData = [
     content:
       "Don’t miss the chance to party with the stars! Our upcoming celebrity night will feature live performances by renowned artists, special guest appearances, and plenty of opportunities to meet your favorite celebrities. Enjoy a VIP experience and take part in exclusive meet-and-greet sessions. Limited tickets available, so act fast!",
     img: eventImg3,
+    date: "December 12, 2024",
   },
   {
     id: 4,
@@ -44,6 +47,7 @@ const eventData = [
     content:
       "Step into a winter wonderland and celebrate the holiday season at our annual gala. Enjoy gourmet dining, live performances, and festive decorations that capture the magic of the season. Perfect for families, friends, or corporate gatherings, this event is sure to get you in the holiday spirit!",
     img: eventImg4,
+    date: "December 25, 2024",
   },
 ];
 
@@ -69,7 +73,7 @@ export default function Events() {
           <h3>{t("footer.events")}</h3>
           <div className="divider"></div>
 
-          <Row>
+          <Row className="mt-3">
             {eventData.map((blog) => (
               <Col key={blog.id} lg={3} md={4} sm={6} className="mb-4">
                 <Card className="h-100">
@@ -79,8 +83,11 @@ export default function Events() {
                       <Card.Title className="mt-3">{blog.title}</Card.Title>
                       <Card.Text className="mt-2">{blog.description}</Card.Text>
                     </div>
+                    <div className="event-date mt-3">
+                      <strong>{blog.date}</strong>
+                    </div>
                     <button
-                      className="blogBtn mt-auto"
+                      className="blogBtn mt-3"
                       onClick={() => handleShow(blog)}
                     >
                       View Event Details
