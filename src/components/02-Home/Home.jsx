57af2124b092799b74a3448aaf5c258fdfa87267
IONHAX04 <thirukumara04@gmail.com>
2024-10-22 12:50:53 +0530

    Title updated

--- src/components/02-Home/Home.jsx
+++ src/components/02-Home/Home.jsx
@@ -16,7 +16,9 @@ export default function Home() {
   return (
     <div className="homeContainer">
       <Helmet>
-        <title>Home - Crystal Club & Lounge</title>
+        <title>
+          Home - Crystal Club Zurich: Premier Nightlife & Exclusive Events
+        </title>
         <meta name="description" content="Crystal Club & Lounge." />
         <link rel="canonical" href="https://crystalclub.ch/" />
 
@@ -24,13 +26,13 @@ export default function Home() {
 
         <meta
           name="keywords"
-          content="club, crystal club, crystal, home, about, girls, gallery, club, events, drinks, blogs, jobs, crystal club home, crystal club and lounge, crystal club and lounge home,, europe, girls, german, girls club, private party, bachelors party, birthday party, private dance"
+          content="club, crystal club, crystal, home, about, girls, gallery, club, events, drinks, blogs, jobs, crystal club home, crystal club and lounge, crystal club and lounge home,, europe, girls, german, girls club, private party, bachelors party, birthday party, private dance,  crystal club, private dance, birthday, bac party, party bachelor, party bachelor party"
         />
         <meta name="robots" content="index, follow" />
 
         <meta
           name="description"
-          content="Experience unforgettable evenings full of elegance, sensuality and first-class entertainment."
+          content="Experience Zurich's elite nightlife at Crystal Club. Enjoy exclusive events, VIP service, and private dance performances in a luxurious, vibrant atmosphere."
         />
       </Helmet>
       {/* Background video */}
@@ -87,7 +89,6 @@ export default function Home() {
             {t("home.never")}
           </h2>
         </section> */}
-
       </div>
     </div>
   );
--- src/components/09-Jobs/Jobs.jsx
+++ src/components/09-Jobs/Jobs.jsx
@@ -78,19 +78,21 @@ export default function Jobs() {
   return (
     <div className="joinUs">
       <Helmet>
-        <title>Jobs - Crystal Club & Lounge</title>
+        <title>
+          Jobs - Crystal Club Zurich: Premier Nightlife & Exclusive Events
+        </title>
         <meta name="description" content="Crystal Club & Lounge." />
         <link rel="canonical" href="https://crystalclub.ch/" />
 
         <meta
           name="keywords"
-          content="club, crystal club, crystal, home, about, girls, gallery, club, events, drinks, blogs, jobs, crystal club home, crystal club and lounge, crystal club and lounge home,, europe, girls, german, girls club, private party, bachelors party, birthday party, private dance"
+          content="club, crystal club, crystal, home, about, girls, gallery, club, events, drinks, blogs, jobs, crystal club home, crystal club and lounge, crystal club and lounge home,, europe, girls, german, girls club, private party, bachelors party, birthday party, private dance, crystal club, private dance, birthday, bac party, party bachelor, party bachelor party"
         />
         <meta name="robots" content="index, follow" />
 
         <meta
           name="description"
-          content="Experience unforgettable evenings full of elegance, sensuality and first-class entertainment."
+          content="Experience Zurich's elite nightlife at Crystal Club. Enjoy exclusive events, VIP service, and private dance performances in a luxurious, vibrant atmosphere."
         />
       </Helmet>
       <section className="joinUsContents">
